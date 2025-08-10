2fd8fe95d612eb5209a7d6b20310577fde35a435
Game-Neek <33062785+Game-Neek@users.noreply.github.com>
2025-08-10 15:35:26 -0400

    Update script.js
    Added an event listener so that when a button click happens itll change the page

--- Weight loss tracker/script.js
+++ Weight loss tracker/script.js
@@ -1,1 +1,8 @@
+let btnClick = document.querySelector("button");
+let enterClick = document.querySelectorAll("");
+
+btnClick.addEventListener("click", () =>{
+    window.location.href = "Calendar.html";
+});
+
 
